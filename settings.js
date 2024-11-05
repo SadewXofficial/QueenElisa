217188b8333e5e18bfa7d34d9e7389e823e79bbf
SadewXofficial <160341086+SadewXofficial@users.noreply.github.com>
2024-11-05 19:44:54 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: 'mongodb+srv://chuutiya309:<ldQNSiK8uSivRslU>@cluster0.jx8wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
+  MONGODB_URL: 'mongodb+srv://chuutiya309:ldQNSiK8uSivRslU@cluster0.jx8wm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,

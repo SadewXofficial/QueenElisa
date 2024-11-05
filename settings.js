223720839c0850c3bae632da71dd5b807acca65c
SadewXofficial <160341086+SadewXofficial@users.noreply.github.com>
2024-11-05 18:42:29 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 Qᴜᴇᴇɴ ᴇʟɪꜱᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: 'mongodb+srv://Sadewofficial:<A44Cc8C1>@cluster0.noyfcuy.mongodb.net/', 
+  MONGODB_URL: '', 
   PREFIX: '.$&',
   LANGUAGE: 'EN',
   PORT: 3000,
